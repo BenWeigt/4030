9f1a14de057101ef0b7194e60b54ad5548636e11
Benjamin Weigt <benweigt@gmail.com>
2020-03-24 22:20:26 +1000

    Milestone 1 q&a

--- R/twitter.R
+++ R/twitter.R
@@ -10,17 +10,19 @@ my_api_secret = 'kbGy21A943rUpXT1gpwDFRppL7tPlXMrlJkykP3GCz0QuNSfEr'
 my_access_token = '894681929038962688-cKu4LfqHVkzdFyJE8FSG3AS0R5ZI02a'
 my_access_token_secret = 'd1XwVHPmoiu2cbiBtPFB7zA4Ts4kH1tn5Alu6MlwIT6iJ'
 
-# Authenticate and get data
-myTwitterData = Authenticate(
-  'twitter',
-  appName=appname,
-  apiKey=my_api_key,
-  apiSecret=my_api_secret,
-  accessToken=my_access_token,
-  accessTokenSecret=my_access_token_secret,
-  useCachedToken = F
-) %>%
-  Collect(searchTerm='-filter:retweets Death Cab for Cutie', language='en', numTweets=18000, writeToFile=FALSE)
+# Authenticate and get data (disabled so as to not accidentaly overwrite our working set)
+if (false) {
+  myTwitterData = Authenticate(
+    'twitter',
+    appName=appname,
+    apiKey=my_api_key,
+    apiSecret=my_api_secret,
+    accessToken=my_access_token,
+    accessTokenSecret=my_access_token_secret,
+    useCachedToken = F
+  ) %>%
+    Collect(searchTerm='-filter:retweets Death Cab for Cutie', language='en', numTweets=18000, writeToFile=FALSE)
+}
 
 
 
@@ -28,11 +30,13 @@ myTwitterData = Authenticate(
 g_twitter_actor_network = myTwitterData %>% Create('actor')
 g_twitter_actor_graph = Graph(g_twitter_actor_network)
 V(g_twitter_actor_graph)$name = V(g_twitter_actor_graph)$screen_name
+V(g_twitter_actor_graph)$label = V(g_twitter_actor_graph)$screen_name
 
 
 # Run Page Rank Algorithm to Find Important Users
 pagerank_actor = sort(page.rank(g_twitter_actor_graph)$vector, decreasing=TRUE)
-head(pagerank_actor, n=3)
+head(pagerank_actor, n=10)
+
 # Create Semantic Network and Graph (and Write to File)
 g_twitter_semantic_network = myTwitterData %>% Create('semantic', stopwordsEnglish = T)
 g_twitter_semantic_graph = Graph(g_twitter_semantic_network)
@@ -47,9 +51,9 @@ g_twitter_semantic_network_50_allterms = myTwitterData %>%
   Create('semantic', termFreq=50, removeTermsOrHashtags=c('Death Cab for Cutie'))
 g_twitter_semantic_network_50_allterms = Graph(g_twitter_semantic_network_allTerms)
 
-
-save(myTwitterData, file='../data/R/twitter/tweets.Rda')
-write(jsonlite::toJSON(myTwitterData), '../data/R/twitter/tweets.json')
+# disabled so as to not accidentaly overwrite our working set
+#save(myTwitterData, file='../data/R/twitter/tweets.Rda')
+#write(jsonlite::toJSON(myTwitterData), '../data/R/twitter/tweets.json')
 write.graph(g_twitter_actor_graph, '../data/R/twitter/twitter_actors.graphml', format='graphml')
 write.graph(g_twitter_semantic_graph, '../data/R/twitter/twitter_semantic.graphml', format='graphml')
 write.graph(g_twitter_semantic_network_50_allterms, '../data/R/twitter/twitter_semantic_50_allterms.graphml', format='graphml')
